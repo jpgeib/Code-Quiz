44502b534b3ea2bb6842e49968d23a549277d0f6
James Geib <jpgeib2@gmail.com>
2020-01-29 20:02:43 -0800

    added initial functions, designated sections

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
+
+//DOM elements
 const quizContainer = document.getElementById("quiz");
 const resultsContainer = document.getElementById("results");
 const submitButton = document.getElementById("submit");
 const leaderboardContainer = document.getElementById("leaderboard");
+
+
+//Core functions
+function buildQuiz(){
+
+};
+
+function showResults(){
+
+};
 
