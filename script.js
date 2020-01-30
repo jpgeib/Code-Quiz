ced04527e19d96a81b258ceba2ac05f3926a80f4
James Geib <jpgeib2@gmail.com>
2020-01-29 19:59:39 -0800

    added variables in javascript

---
+++ script.js
@@ -0,0 +1,5 @@
+const quizContainer = document.getElementById("quiz");
+const resultsContainer = document.getElementById("results");
+const submitButton = document.getElementById("submit");
+const leaderboardContainer = document.getElementById("leaderboard");
+
