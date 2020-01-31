0412367aecfa9d018a29ceca249ce4373a8ad1e8
James Geib <jpgeib2@gmail.com>
2020-01-31 13:37:28 -0800

    built buildQuiz function, one error present, looking into reason why

--- script.js
+++ script.js
@@ -12,9 +12,19 @@ function buildQuiz() {
   myQuestions.forEach(function(currentQuestion, questionAnswer) {
     const answers = [];
     for(letter in currentQuestion.answers) {
+      answers.push(
+        <label>
+          <input type="radio" name="question${questionNumber}" value="${letter}"></input>
+          ${letter}:
+          ${currentQuestion.answers[letter]}
+        </label>
+      );
+    };
 
-    }
-  })
+    output.push(<div class="question">${currentQuestion.question}</div><div class="answers">${answers.join("")}</div>);
+  }
+  );
+  quizContainer.innerHTML = output.join("");
 };
 
 function showResults(){
