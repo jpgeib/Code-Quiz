d156330f25860f324aa30b51d6d0b60d4b1d02dd
James Geib <jpgeib2@gmail.com>
2020-01-31 18:26:11 -0800

    figured out the error, pushing the changes

--- script.js
+++ script.js
@@ -46,15 +46,19 @@ function buildQuiz() {
     const answers = [];
     for(letter in currentQuestion.answers) {
       answers.push(
-        
-          
-          
+        `<label>
+            <input type="radio" name="question${questionNumber}" value="${letter}">
+            ${letter} :
+            ${currentQuestion.answers[letter]}
+          </label>`    
       );
     };
 
-    output.push();
-  }
-  );
+    output.push(
+      `<div class="question"> ${currentQuestion.question} </div>
+        <div class="answers"> ${answers.join('')} </div>`
+    );
+  });
   quizContainer.innerHTML = output.join("");
 };
 
