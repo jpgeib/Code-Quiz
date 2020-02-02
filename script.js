d7dda4518ad634073c4697ca77fdfa7cafd632bb
James Geib <jpgeib2@gmail.com>
2020-02-01 20:47:15 -0800

    prototype quiz mostly done, checking why questions won't display individually

--- script.js
+++ script.js
@@ -1,44 +1,3 @@
-
-//DOM elements
-const quizContainer = document.getElementById("quiz");
-const resultsContainer = document.getElementById("results");
-const submitButton = document.getElementById("submit");
-const leaderboardContainer = document.getElementById("leaderboard");
-
-//Questions
-
-const myQuestions = [
-    {
-      question: "Who invented JavaScript?",
-      answers: {
-        a: "Douglas Crockford",
-        b: "Sheryl Sandberg",
-        c: "Brendan Eich"
-      },
-      correctAnswer: "c"
-    },
-    {
-      question: "Which one of these is a JavaScript package manager?",
-      answers: {
-        a: "Node.js",
-        b: "TypeScript",
-        c: "npm"
-      },
-      correctAnswer: "c"
-    },
-    {
-      question: "Which tool can you use to ensure code quality?",
-      answers: {
-        a: "Angular",
-        b: "jQuery",
-        c: "RequireJS",
-        d: "ESLint"
-      },
-      correctAnswer: "d"
-    }
-  ];
-
-
 //Core functions
 function buildQuiz() {
  const output = [];
@@ -87,12 +46,94 @@ function showResults(){
   resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
 };
 
+//Page functions
+
+function showSlide(n) {
+  slides[currentSlide].classList.remove('active-slide');
+  slides[n].classList.add('active-slide');
+  currentSlide = n;
+
+  if(currentSlide === 0) {
+    previousButton.style.display = 'none';
+  } else {
+    previousButton.style.display = 'inline-block';
+  };
+
+  if(currentSlide === slides.length - 1) {
+    nextButton.style.display = 'none';
+    submitButton.style.display = 'inline-block';
+  } else {
+    nextButton.style.display = 'inline-block';
+    submitButton.style.display = 'none';
+  };
+};
+
+function showNextSlide() {
+  showSlide(currentSlide + 1);
+};
+
+function showPreviousSlide() {
+  showSlide(currentSlide - 1);
+};
+
+
+//DOM elements
+const quizContainer = document.getElementById("quiz");
+const resultsContainer = document.getElementById("results");
+const submitButton = document.getElementById("submit");
+const leaderboardContainer = document.getElementById("leaderboard");
+
+//Questions
+
+const myQuestions = [
+    {
+      question: "Who invented JavaScript?",
+      answers: {
+        a: "Douglas Crockford",
+        b: "Sheryl Sandberg",
+        c: "Brendan Eich"
+      },
+      correctAnswer: "c"
+    },
+    {
+      question: "Which one of these is a JavaScript package manager?",
+      answers: {
+        a: "Node.js",
+        b: "TypeScript",
+        c: "npm"
+      },
+      correctAnswer: "c"
+    },
+    {
+      question: "Which tool can you use to ensure code quality?",
+      answers: {
+        a: "Angular",
+        b: "jQuery",
+        c: "RequireJS",
+        d: "ESLint"
+      },
+      correctAnswer: "d"
+    }
+  ];
 
 //Initialize the quiz
 
 buildQuiz();
 
-//Show results upon submitting
+//Pagination
+const previousButton = document.getElementById("previous");
+const nextButton = document.getElementById("next");
+const slides = document.querySelectorAll(".slide");
+let currentSlide = 0;
+
+//Show the first slide
+
+showSlide(currentSlide);
+
+
+//Event listeners
 
 submitButton.addEventListener("click", showResults);
+previousButton.addEventListener("click", showPreviousSlide);
+nextButton.addEventListener("click", showNextSlide);
 
