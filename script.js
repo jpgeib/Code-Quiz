2ad4fe506622246b5f5f10522aca9bdc2295f0df
James Geib <jpgeib2@gmail.com>
2020-02-25 17:30:29 -0800

    pseudocode from TA

--- script.js
+++ script.js
@@ -1,46 +1,74 @@
-(function(){ 
+(function () {
 
   //Building the quiz
   function buildQuiz() {
-  const output = [];
+    const output = [];
 
-  myQuestions.forEach(
-    (currentQuestion, questionNumber) => {
-      const answers = [];
-      for(letter in currentQuestion.answers){
-        answers.push(
-          `<label>
+    myQuestions.forEach(
+      (currentQuestion, questionNumber) => {
+        const answers = [];
+        for (letter in currentQuestion.answers) {
+          answers.push(
+            `<label>
             <input type="radio" name="question${questionNumber}" value="${letter}">
             ${letter} :
             ${currentQuestion.answers[letter]}
           </label>`
-        );
-      };
-      
-      output.push(
-        `<div class="slide">
+          );
+        };
+
+        output.push(
+          `<div class="slide">
         <div class="question"> ${currentQuestion.question} </div>
         <div class="answers"> ${answers.join('')} </div>
         </div>`
-      );
-    }
-  );
+        );
+      }
+    );
     quizContainer.innerHTML = output.join('');
   };
 
   //Showing the results
+  let questionIndex = 0;
 
-  function showResults(){
+
+  // function for rendering  questions takes an index
+
+  // using the question, render the 1st question
+  function renderQuestion(index) {
+    const currentQuestion = myQuestions[index];
+    for(let answer in currentQuestion.answers){
+      //  Key to the current question a, b, or c
+      answer
+      // Actual value
+      currentQuestion.answers[answer];
+    }
+
+  }
+
+  // function for checking answer
+  // add event listner to the button
+  // event prevent Default
+  // check to see if questionIndex is within the bounds of your questions Array
+  // set questionIndex = 0
+  // if so, return 
+  // using JS grab the value from the inputs
+  // using the questionIndex variable, check to see if answer is correct
+  // if wrong subract tijme
+  //  else do 
+  // increment questionIndex by 1
+  // call renderQuestion function
+  function showResults() {
     const answerContainers = quizContainer.querySelectorAll('.answers');
 
     let numCorrect = 0;
 
-    myQuestions.forEach( (currentQuestion, questionNumber) => {
+    myQuestions.forEach((currentQuestion, questionNumber) => {
       const answerContainer = answerContainers[questionNumber];
       const selector = `input[name=question${questionNumber}]:checked`;
       const userAnswer = (answerContainer.querySelector(selector) || {}).value;
 
-      if(userAnswer === currentQuestion.correctAnswer) {
+      if (userAnswer === currentQuestion.correctAnswer) {
         numCorrect++;
         answerContainers[questionNumber].style.color = 'lightgreen';
       } else {
@@ -54,11 +82,11 @@
   //Timer functions
 
   function timeLimit(chosenTime) {
-    let timeInterval = setInterval(function() {
+    let timeInterval = setInterval(function () {
       secondsLeft--;
       timer.textContent = secondsLeft + " seconds remaining...";
 
-      if(secondsLeft === 0) {
+      if (secondsLeft === 0) {
         clearInterval(timeInterval);
         alert("Time has run out! Refresh the page to try again.")
       }
@@ -74,13 +102,13 @@
     slides[n].classList.add('active-slide');
     currentSlide = n;
 
-    if(currentSlide === 0) {
+    if (currentSlide === 0) {
       previousButton.style.display = 'none';
     } else {
       previousButton.style.display = 'inline-block';
     };
 
-    if(currentSlide === slides.length - 1) {
+    if (currentSlide === slides.length - 1) {
       nextButton.style.display = 'none';
       submitButton.style.display = 'inline-block';
     } else {
@@ -109,59 +137,59 @@
   //Questions
 
   const myQuestions = [
-      {
-        question: "Who invented JavaScript?",
-        answers: {
-          a: "Douglas Crockford",
-          b: "Sheryl Sandberg",
-          c: "Brendan Eich"
-        },
-        correctAnswer: "c"
+    {
+      question: "Who invented JavaScript?",
+      answers: {
+        a: "Douglas Crockford",
+        b: "Sheryl Sandberg",
+        c: "Brendan Eich"
       },
-      {
-        question: "Which one of these is a JavaScript package manager?",
-        answers: {
-          a: "Node.js",
-          b: "TypeScript",
-          c: "npm"
-        },
-        correctAnswer: "c"
+      correctAnswer: "c"
+    },
+    {
+      question: "Which one of these is a JavaScript package manager?",
+      answers: {
+        a: "Node.js",
+        b: "TypeScript",
+        c: "npm"
       },
-      {
-        question: "Which tool can you use to ensure code quality?",
-        answers: {
-          a: "Angular",
-          b: "jQuery",
-          c: "RequireJS",
-          d: "ESLint"
-        },
-        correctAnswer: "d"
+      correctAnswer: "c"
+    },
+    {
+      question: "Which tool can you use to ensure code quality?",
+      answers: {
+        a: "Angular",
+        b: "jQuery",
+        c: "RequireJS",
+        d: "ESLint"
       },
-      {
-        question: "If we were to write: var someNumber = 5, and someNumber = 7, what would the console log?",
-        answers: {
-          a: "5",
-          b: "7",
-          c: "undefined",
-          d: "42"
-        },
-        correctAnswer: "b"
+      correctAnswer: "d"
+    },
+    {
+      question: "If we were to write: var someNumber = 5, and someNumber = 7, what would the console log?",
+      answers: {
+        a: "5",
+        b: "7",
+        c: "undefined",
+        d: "42"
       },
-      {
-        question: "Which function takes a string and converts it to an array?",
-        answers: {
-          a: ".split()",
-          b: ".join()",
-          c: ".reverse()",
-          d: ".push()"
-        },
-        correctAnswer: "a"
-      }
-    ];
+      correctAnswer: "b"
+    },
+    {
+      question: "Which function takes a string and converts it to an array?",
+      answers: {
+        a: ".split()",
+        b: ".join()",
+        c: ".reverse()",
+        d: ".push()"
+      },
+      correctAnswer: "a"
+    }
+  ];
 
   //Initialize the quiz
   buildQuiz();
-  
+
 
   //Pagination
   const previousButton = document.getElementById("previous");
@@ -179,7 +207,7 @@
   submitButton.addEventListener("click", showResults);
   previousButton.addEventListener("click", showPreviousSlide);
   nextButton.addEventListener("click", showNextSlide);
-  
+
 
 })();
 
