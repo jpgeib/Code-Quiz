08a595f4ad44cc92f3abb39b28de34c15cb3b8ae
James Geib <jpgeib2@gmail.com>
2020-03-26 12:37:19 -0700

    finished leaderboard.js and edited leaderboard.html, going to experiment further once I have a functioning product

---
+++ leaderboard.js
@@ -0,0 +1,27 @@
+//Leaderboard functions
+
+const createLeaderboard = function() {
+    const scores = JSON.parse(window.localStorage.getItem("leaderboard")) || [];
+
+    scores.sort(function(a, b) {
+        return b.score - a.score;
+    });
+
+    scores.forEach(function(result) {
+        const leaderboardSlot = document.createElement("li");
+        leaderboardSlot.textContent = result.initials + " - " + result.score;
+
+        const leaderboardEl = document.getElementById("leaderboard");
+        leaderboardEl.appendChild(leaderboardSlot);
+
+    });
+};
+
+const clearScores = function() {
+    window.localStorage.removeItem("leaderboard");
+    window.location.reload();
+}
+
+document.getElementById("clear").onClick = clearScores;
+
+createLeaderboard();
